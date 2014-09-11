910d453d2e82da7508bcd10ead8016736738e754
Javatechig <javatechig@gmail.com>
2014-09-12 00:10:09 +0100

    Added Export to Excel functionality to TODO

--- TODO Android App/src/com/javatechig/todo/TodoListActivity.java
+++ TODO Android App/src/com/javatechig/todo/TodoListActivity.java
@@ -1,9 +1,20 @@
 package com.javatechig.todo;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.widget.SimpleCursorAdapter;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +22,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class TodoListActivity extends ActionBarActivity {
@@ -24,16 +36,17 @@ public class TodoListActivity extends ActionBarActivity {
 
 		dbcon = new SQLController(this);
 		dbcon.open();
+		
 		listView = (ListView) findViewById(R.id.list_view);
 		listView.setEmptyView(findViewById(R.id.empty));
 		// Attach The Data From DataBase Into ListView Using Crusor Adapter
+		
 		Cursor cursor = dbcon.fetch();
+		
 		String[] from = new String[] { DBhelper._ID, DBhelper.TODO_SUBJECT,  DBhelper.TODO_DESC};
 		int[] to = new int[] { R.id.id, R.id.title, R.id.desc };
-		
 
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.activity_view_record, cursor,
-				from, to);
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.activity_view_record, cursor, from, to);
 
 		adapter.notifyDataSetChanged();
 		listView.setAdapter(adapter);
@@ -73,7 +86,82 @@ public class TodoListActivity extends ActionBarActivity {
 			startActivity(add_mem);
 			return true;
 		}
+		else if (id == R.id.export_records) {
+			Cursor cursor = dbcon.fetch();
+			
+			exportToExcel(cursor);
+			
+			return true;
+		}
 		return super.onOptionsItemSelected(item);
 	}
 
+	private void exportToExcel(Cursor cursor) {
+		
+		final String fileName = "TodoList.xls";
+		
+		//Saving file in external storage
+		File sdCard = Environment.getExternalStorageDirectory();
+		
+		File directory = new File(sdCard.getAbsolutePath() + "/javatechig.todo");
+		
+		//create directory if not exist
+		if(!directory.isDirectory()){
+			directory.mkdirs();	
+		}
+		
+		//file path
+		File file = new File(directory, fileName);
+		
+		WorkbookSettings wbSettings = new WorkbookSettings();
+		wbSettings.setLocale(new Locale("en", "EN"));
+		
+		WritableWorkbook workbook;
+		
+		try {
+			workbook = Workbook.createWorkbook(file, wbSettings);
+			
+			//Excel sheet name. 0 represents first sheet
+			WritableSheet sheet = workbook.createSheet("MyShoppingList", 0);
+			
+			//Supply row and column index
+			Label label0 = new Label(0, 0, "Subject");
+			Label label1 = new Label(1, 0, "Description");
+
+			try {
+				sheet.addCell(label0);
+				sheet.addCell(label1);
+				
+				if (cursor.moveToFirst()) {
+					do {
+						String title = cursor.getString(cursor.getColumnIndex(DBhelper.TODO_SUBJECT));
+						String desc = cursor.getString(cursor.getColumnIndex(DBhelper.TODO_DESC));
+
+						int i = cursor.getPosition() + 1;
+						
+						sheet.addCell(new Label(0, i, title));
+						sheet.addCell(new Label(1, i, desc));
+					} while (cursor.moveToNext());
+				}
+				
+				//closing cursor
+				cursor.close();
+					
+			} catch (RowsExceededException e) {
+				e.printStackTrace();
+			} catch (WriteException e) {
+				e.printStackTrace();
+			}
+			
+			workbook.write();
+			
+			try {
+				workbook.close();
+			} catch (WriteException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
